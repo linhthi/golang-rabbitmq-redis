a7564d3ccaf295d8e4dc2930ee6e7636ffb1fd13
Linh Hoang <hoanglinh831999@gmail.com>
2020-07-22 10:23:25 +0700

    dequeue and caching in redis

--- receive.go
+++ receive.go
@@ -33,12 +33,12 @@ func main() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-	"test", // name
-	true,   // durable
-	false,   // delete when unused
-	false,   // exclusive
-	false,   // no-wait
-	nil,     // arguments
+		"test", // name
+		true,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
